412d8d5c80aadecb98efee5c4a773c0c4cfe2dfa
unknown <leejong7492@naver.com>
2024-07-26 17:36:22 +0900

    a

--- AI_cover/settings.py
+++ AI_cover/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$l6k506mffapyoca&17^q85b2y=m6oktd5n4xc(k6^!0ip!4yj'
+SECRET_KEY = os.getenv("SECRET_KEY_AI_COVER")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
